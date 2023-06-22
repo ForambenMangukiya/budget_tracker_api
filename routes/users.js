3be00b811891930b9536407710796b7224924cca
Hani <hanikahhaleh@gmail.com>
2023-06-22 14:05:36 +0200

    changed the update method for users, so it can accept specific information only ,, added filter feature for the transactions

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,27 @@
 const express = require("express");
 
-const { updateUser, deleteOneUser, loginUser, signupUser } = require("../controllers/users");
+const {
+  updateUser,
+  deleteOneUser,
+  loginUser,
+  signupUser,
+  budget,
+} = require("../controllers/users");
 
 const api = express.Router();
 
-//login 
+//login
 api.post("/login", loginUser);
 
-//signup 
+//signup
 api.post("/signup", signupUser);
 
 // DEFAULT ROUTE   ---- PATH TO create a new user
 // api.route("/").post(newUser);
 
 // Path to update or to delete user
-api.route("/:id").put(updateUser).delete(deleteOneUser);
+api.route("/:id").put(updateUser).delete(deleteOneUser).get(budget);
+
+// Path to get budgets from one User
 
 module.exports = api;
