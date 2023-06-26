ef678e0c6ed6ac8e89b442ce5118ca0e08c7a279
Anton <avonpoblozki@posteo.de>
2023-06-26 10:21:56 +0200

    ADD DeleteAll Route to clean transactions DB

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const requireAuth = require("../middlewares/requireAuth")
+const requireAuth = require("../middlewares/requireAuth");
 
 const {
   newTransaction,
   deleteTransaction,
   getAllTransaction,
   updateTransaction,
+  deleteAllTransactions,
 } = require("../controllers/transactions");
 
 const api = express.Router();
@@ -13,10 +14,13 @@ const api = express.Router();
 api.use(requireAuth);
 
 // ROUTE : Get All Transactions and Create Transaction
-api.route("/").get(getAllTransaction).post(newTransaction);
+api
+  .route("/")
+  .get(getAllTransaction)
+  .post(newTransaction)
+  .delete(deleteAllTransactions);
 
 // ROUTE : Delete One Transaction and Update One Transaction
 api.route("/:id").delete(deleteTransaction).put(updateTransaction);
-
 
 module.exports = api;
