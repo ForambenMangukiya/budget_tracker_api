f9e7e85747ead09ec03fce64b19a4931745c4e6c
gretchelg <gretchelglopez@gmail.com>
2023-06-29 09:41:37 +0200

    save plaid txns to mongoDB

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -47,18 +47,21 @@ const deleteTransaction = async (req, res) => {
 
 // function getAll Transactions for a user
 const getAllTransaction = async (req, res) => {
+  console.log("get all trans")
   try {
     const { timeperiod } = req.query;
     const user = req.user._id;
     if (timeperiod === "all") {
       const user = req.user._id;
       const transactions = await Transaction.find({ user });
+      console.log("display all transactions by user:", transactions)
       res.status(200).json(transactions);
     } else {
       const user = req.user._id;
       let query = {};
       const currentDate = new Date();
       if (timeperiod === "month") {
+        console.log("by month")
         const startOfmonth = new Date(
           currentDate.getFullYear(),
           currentDate.getMonth(),
@@ -66,6 +69,7 @@ const getAllTransaction = async (req, res) => {
         );
         query = { user, tran_date: { $gte: startOfmonth, $lte: currentDate } };
       } else if (timeperiod === "3months") {
+        console.log("3 month")
         const last3months = new Date(
           currentDate.getFullYear(),
           currentDate.getMonth() - 3,
@@ -73,6 +77,7 @@ const getAllTransaction = async (req, res) => {
         );
         query = { user, tran_date: { $gte: last3months, $lte: currentDate } };
       } else if (timeperiod === "6months") {
+        console.log("6 month")
         const last6months = new Date(
           currentDate.getFullYear(),
           currentDate.getMonth() - 6,
@@ -80,9 +85,11 @@ const getAllTransaction = async (req, res) => {
         );
         query = { user, tran_date: { $gte: last6months, $lte: currentDate } };
       } else if (timeperiod === "year") {
+        console.log("by year")
         const startOfYear = new Date(currentDate.getFullYear(), 1, 1);
         query = { tran_date: { $gte: startOfYear, $lte: currentDate } };
       } else if (timeperiod === "week") {
+        console.log("by week")
         const lastWeek = new Date(
           currentDate.getFullYear(),
           currentDate.getMonth(),
@@ -97,6 +104,7 @@ const getAllTransaction = async (req, res) => {
       }
       console.log("req.user._id:", req.user._id);
       const transactions = await Transaction.find(query);
+      console.log("filter by date:", transactions )
       res.status(200).json(transactions);
     }
   } catch (error) {
