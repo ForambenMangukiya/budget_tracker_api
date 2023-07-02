bed97d09a8b0f701d1e80c43bbd6d6eb0d6a64b3
gretchelg <gretchelglopez@gmail.com>
2023-07-02 09:52:37 +0200

    fix transync issue

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || "DE").split(
   ","
 );
 const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || "";
+const RECORDING_MODE = process.env.RECORDING_MODE || false
 // TODO: save the following to mongoDB user!!!!
 let ACCESS_TOKEN = null;
 let PUBLIC_TOKEN = null;
@@ -81,7 +82,7 @@ app.post("/api/create_link_token/:_id", async function (req, res) {
       client_user_id: _id,
     },
     client_name: "Plaid Test App",
-    products: ["transactions"],
+    products: ["transactions","auth"],
     language: "en",
     webhook: "https://webhook.example.com",
     country_codes: ["DE"],
@@ -106,6 +107,7 @@ app.post("/api/set_access_token", async function (req, res, next) {
     ACCESS_TOKEN = tokenResponse.data.access_token;
     ITEM_ID = tokenResponse.data.item_id;
     console.log(">>>>ITEM_ID", ITEM_ID);
+    console.log(">>>>ACCESS_TOKEN", ACCESS_TOKEN);
     // if (PLAID_PRODUCTS.includes(Products.Transfer)) {
     //     const TRANSFER_ID = await authorizeAndCreateTransfer(ACCESS_TOKEN);
     // }
@@ -122,6 +124,7 @@ app.post("/api/set_access_token", async function (req, res, next) {
 
 // Retrieve Transactions for an Item
 app.get("/api/transactions/:id", async function (req, res, next) {
+  console.log(">>>>ACCESS TOKEN:", ACCESS_TOKEN)
   const { id } = req.params;
   try {
     // Set cursor to empty to receive all historical updates
@@ -141,45 +144,18 @@ app.get("/api/transactions/:id", async function (req, res, next) {
         cursor: cursor,
       };
 
-      const getTransactions = async () =>
-        Promise.resolve().then(async () => {
-          const SLEEP_TIME = 10000;
-          const MAX_RETRY = 3;
+      // const response = await fetchTransactionsFromPlaid();
+      // const data = response.data;
 
-          var retries = MAX_RETRY;
-          while (retries > 0) {
-            console.log(
-              "INFO: inside retryer function, retries remaining=",
-              retries
-            );
+      let data
+      console.log("recording mode?", {RECORDING_MODE})
+      if (RECORDING_MODE) {
+        console.log("recording is starting", id)
+        data = getStaticPlaidTransactions()
+      } else {
+        data = await fetchTransactionsFromPlaid()
+      }
 
-            try {
-              const response = await client.transactionsSync(request);
-              if (response.data.added.length > 0) {
-                console.log("INFO: success! found non-zero array!");
-                return response;
-              }
-              console.log(
-                "WARNING: transactionsSync returned ok but had empty array!"
-              );
-            } catch (e) {
-              console.log("ERROR: transactionsSync failed:", e);
-            }
-
-            console.log("INFO: sleeping before next retry...");
-            await new Promise((resolve) => {
-              setTimeout((_) => {
-                resolve();
-              }, SLEEP_TIME); // sleep for N seconds
-            });
-
-            console.log("INFO: ...woken up; trying again");
-            retries = retries - 1;
-          }
-        });
-
-      const response = await getTransactions();
-      const data = response.data;
       // console.log("trans:", data);
       // Add this page of results
       added = added.concat(data.added);
@@ -190,28 +166,69 @@ app.get("/api/transactions/:id", async function (req, res, next) {
       cursor = data.next_cursor;
     }
 
+
     const compareTxnsByDateAscending = (a, b) =>
       (a.date > b.date) - (a.date < b.date);
     // Return the 8 most recent transactions
     const recently_added = [...added]
       .sort(compareTxnsByDateAscending)
-      .slice(-4);
-    // console.log("ALL added:", added )  
+      .slice(-5);
+    console.log("ALL added:", added )
+    console.log(">>>>ACCESS_TOKEN", ACCESS_TOKEN);  
 
-    recently_added.map(async addedTransaction => {
+    // recently_added.map(async addedTransaction => {
+    added.map(async addedTransaction => {
       const newTransaction = getTransactionFromImportedTxn(addedTransaction, id)
       newTransaction.category_name = mapCategoryFromBank(addedTransaction.category)
       await updateOrInsertTxn(newTransaction)  
     })
-
-    // res.status(200).json({ all_transactions: added });
-    res.status(200).json({ latest_transactions: recently_added });
+    console.log(">>>>ACCESS_TOKEN", ACCESS_TOKEN);
+    res.status(200).json({ all_transactions: added });
+    // res.status(200).json({ latest_transactions: recently_added });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
 const Transaction = require("./schemas/Transactions");
+
+async function fetchTransactionsFromPlaid() {
+  Promise.resolve().then(async () => {
+    const SLEEP_TIME = 10000;
+    const MAX_RETRY = 3;
+
+    var retries = MAX_RETRY;
+    while (retries > 0) {
+      console.log(
+        "INFO: inside retryer function, retries remaining=",
+        retries
+      );
+
+      try {
+        const response = await client.transactionsSync(request);
+        if (response.data.added.length > 0) {
+          console.log("INFO: success! found non-zero array!");
+          return response.data;
+        }
+        console.log(
+          "WARNING: transactionsSync returned ok but had empty array!"
+        );
+      } catch (e) {
+        console.log("ERROR: transactionsSync failed:", e);
+      }
+
+      console.log("INFO: sleeping before next retry...");
+      await new Promise((resolve) => {
+        setTimeout((_) => {
+          resolve();
+        }, SLEEP_TIME); // sleep for N seconds
+      });
+
+      console.log("INFO: ...woken up; trying again");
+      retries = retries - 1;
+    }
+  })
+};
 
 async function updateOrInsertTxn(newTransaction) {
   const foundTxn = await findTransactionByTranID(newTransaction.tran_id)
@@ -267,7 +284,7 @@ function mapCategoryFromBank(category){
   if (category.length == 0) {
     return "others"
   }
-
+  console.log("plaid category:", category)
   // TODO lowercase the category
   const lowercasedCategory = category[0]
 
@@ -288,11 +305,80 @@ function getTransactionFromImportedTxn(importedTxn, id) {
       category_name: importedTxn.category,
       tran_description: importedTxn.name,
       tran_amount: importedTxn.amount,
-      tran_sign: "DR", 
-      tran_currency: "DE", 
+      tran_sign: importedTxn.tran_sign ? importedTxn.tran_sign : "DR", 
+      tran_currency: importedTxn.tran_currency ? importedTxn.tran_currency : "DE", 
       tran_date: importedTxn.authorized_date,
       user: id, 
       tran_id: importedTxn.transaction_id,
   }
   return txn
 }
+
+function getStaticPlaidTransactions(){
+  console.log("getStaticPlaidTransactions()")
+  data = {
+    added: [
+      {
+        category: ["medicine", "health"],
+        name: "KOENIGIN LUISE APOTHEK/BERLIN/DE",
+        amount: 26.37, 
+        tran_sign: "DR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-16T00:00:00.000Z",
+        transaction_id: "4673956022",
+      },
+      {
+        category: ["deposits", "transfer"],
+        name: "SEPA-Payment Transfer",
+        amount: 400.00, 
+        tran_sign: "CR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-29T00:00:00.000Z",
+        transaction_id: "21005000",
+      },
+      {
+        category: ["work", "shopping"],
+        name: "AMZN Mktp DE.H21I86JR5//800-279-6620/LU",
+        amount: 22.58, 
+        tran_sign: "DR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-15T00:00:00.000Z",
+        transaction_id: "AZxVGdvjJxtwREJjnKWAspNwLR5WMau9D4A3V",
+      },
+      {
+        category: ["savings", "transfer"],
+        name: "SEPA-Payment Transfer",
+        amount: 200.00, 
+        tran_sign: "CR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-14T00:00:00.000Z",
+        transaction_id: "rGgDae4bvgS9LyjwoGxZIv9mpag1Wlc74d6No",
+      },
+      {
+        category: ["communication", "bills"],
+        name: "Gebuehr fuer o2 my Prepaid S fuer Ihr Prepaid-Konto",
+        amount: 9.99, 
+        tran_sign: "DR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-09T00:00:00.000Z",
+        transaction_id: "W7LMQber3Liz9JQwEk46CqMAy4gGRwU68P1q5",
+      },
+      {
+        category: ["insurance", "health"],
+        name: "SEPA-Direct Debit Feather by Stripe via PPRO",
+        amount: 9.00, 
+        tran_sign: "DR", 
+        tran_currency: "DE",
+        authorized_date: "2023-06-06T00:00:00.000Z",
+        transaction_id: "13005000",
+      },
+    ],
+    modified: [],
+    removed: [],
+    hasMore: false,
+    next_cursor: null
+  }
+  console.log("EXITING getStaticPlaidTransactions", data)
+  return data
+}
+
